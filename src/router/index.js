c106a5f963d2d888c60319ab70ccc8aefeee7a9a
TarKz06 <khematat_s@elearning.cmu.ac.th>
2021-07-24 14:21:56 +0700

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,48 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import EventList from '../views/EventList.vue'
+import About from '../views/About.vue'
+import EventDetails from '../views/event/Details.vue'
+import EventRegister from '../views/event/Register.vue'
+import EventEdit from '../views/event/Edit.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'EventList',
+    component: EventList,
+    props: (route) => ({
+      page: parseInt(route.query.page) || 1,
+      perPage: parseInt(route.query.perPage) || 1
+    })
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+  {
+    path: '/event/id',
+    name: 'EventDetails',
+    component: EventDetails,
+    props: true
+  },
+  {
+    path: '/event/:id/register',
+    name: 'EventRegister',
+    component: EventRegister,
+    props: true
+  },
+  {
+    path: '/event/:id/edit',
+    name: 'EventEdit',
+    component: EventEdit,
+    props: true
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/EventList.vue
@@ -0,0 +1,105 @@
+<template>
+  <h1>Events For Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <div class="pagination">
+      <router-link
+        id="page-prev"
+        :to="{ name: 'EventList', query: { page: page - 1 } }"
+        rel="prev"
+        v-if="page != 1"
+      >
+        Prev page
+      </router-link>
+      <router-link
+        id="page-next"
+        :to="{ name: 'EventList', query: { page: page + 1 } }"
+        rel="next"
+        v-if="hasNextPage"
+      >
+        Next page
+      </router-link>
+    </div>
+    <select id="checkPage" v-model.number="checkPage">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+      <option>6</option>
+    </select>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
+import { watchEffect } from '@vue/runtime-core'
+// import axios from 'axios'
+export default {
+  name: 'EventList',
+  props: {
+    page: {
+      type: Number,
+      required: true
+    },
+    perPage: {
+      type: Number,
+      required: true
+    }
+  },
+  components: {
+    EventCard // register it as a child component
+  },
+  data() {
+    return {
+      events: null,
+      totalEvents: 0, // <-- Added this to store totalEvent
+      checkPage: 1
+    }
+  },
+  created() {
+    watchEffect(() => {
+      EventService.getEvents(this.perPage, this.page)
+        .then((response) => {
+          this.events = response.data
+          this.totalEvents = response.headers['x-total-count']
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    })
+  },
+  computed: {
+    hasNextPage() {
+      //fisrt , calulate total page
+      let totalPages = Math.ceil(this.totalEvents / this.perPage) // 2is event per page
+      //Then Check to see if the current page is less then total pages
+      return this.page < totalPages
+    }
+  }
+}
+</script>
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.pagination {
+  display: flex;
+  width: 290px;
+}
+.pagination a {
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+#page-prev {
+  text-align: left;
+}
+#page-next {
+  text-align: right;
+}
+</style>
