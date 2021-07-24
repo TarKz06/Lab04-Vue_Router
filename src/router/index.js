3517b4731b2bc73f30057bd74665338dde582188
TarKz06 <khematat_s@elearning.cmu.ac.th>
2021-07-24 21:48:04 +0700

    5.5-5,6

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import EventDetails from '../views/event/Details.vue'
 import EventRegister from '../views/event/Register.vue'
 import EventEdit from '../views/event/Edit.vue'
 import EventLayout from '../views/event/Layout.vue'
+import NotFound from '../views/NotFound.vue'
 const routes = [
   {
     path: '/',
@@ -45,6 +46,17 @@ const routes = [
         component: EventEdit
       }
     ]
+  },
+  {
+    path: '/404/:resource',
+    name: '404Resource',
+    component: NotFound,
+    props: true
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound
   }
 ]
 
